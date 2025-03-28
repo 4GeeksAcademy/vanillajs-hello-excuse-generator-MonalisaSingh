e826999177b82a9f6904b2ca484fce4342c14fa9
monalisasingh87 <monalisa.singh87@gmail.com>
2025-03-28 01:24:06 +0000

    redoing my excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who=["My grandma","My dog","My mailman","My bird"];
+  let action=[" ate "," crushed "," broke "," destroyed"];
+  let what=[" my car "," my homework"," my phone"];
+  let when=[" before class started."," while i was sleeping."," during lunchtime."];
+
+  let selectedWho = Math.floor(Math.random() * who.length)
+  let selectedAction = Math.floor(Math.random() * action.length)
+  let selectedWhat = Math.floor(Math.random() * what.length)
+  let selectedWhen = Math.floor(Math.random() * when.length)
+
+  let statement = who[selectedWho] + action[selectedAction] + what[selectedWhat] + when[selectedWhen];
+  
+  let htmlStatement = document.querySelector('.excuse');
+  htmlStatement.innerHTML = statement;
+  
+  
 };
